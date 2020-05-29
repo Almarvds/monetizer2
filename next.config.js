5a6cb740caa1fb41eb52b3225a5ce9dc7fbe8b3d
Almarvds <a.r.v.d.stappen@student.tue.nl>
2020-05-29 13:50:46 +0200

    package changes and http changes

--- next.config.js
+++ next.config.js
@@ -20,6 +20,6 @@ module.exports = {
     return config
   },
   env: {
-    server_url: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://104.155.33.213/'
+    server_url: process.env.NODE_ENV === 'development' ? 'https://localhost:8080/' : 'https://104.155.33.213/'
   }
 }
